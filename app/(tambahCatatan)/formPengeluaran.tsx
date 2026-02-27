b5fade73ac9964daf69e597f6b376d56feef9782
mardin ajalah <tredingb25@gmail.com>
2026-02-27 21:12:10 +0700

    revactor: add folder home

--- app/(tambahCatatan)/formPengeluaran.tsx
+++ app/(tambahCatatan)/formPengeluaran.tsx
@@ -1,12 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
 const FormPengeluaran = () => {
   return (
-    <SafeAreaView>
+    <View>
       <Text>FormPengeluaran</Text>
-    </SafeAreaView>
+    </View>
   )
 }
 
