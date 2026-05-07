3812e0f14d3c1caf148da1ad7886595102954703
mardin ajalah <tredingb25@gmail.com>
2026-05-07 20:20:19 +0700

    Use production API and add offline guard

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,18 @@ import { Stack } from 'expo-router';
 import { TransactionsProvider } from '@/components/TransactionsStore';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import NetworkGuard from '@/components/NetworkGuard';
 import "@/global.css";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <TransactionsProvider>
-        <StatusBar barStyle='light-content' />
-        <Stack screenOptions={{ headerShown: false }} />
-      </TransactionsProvider>
+      <NetworkGuard>
+        <TransactionsProvider>
+          <StatusBar barStyle='light-content' />
+          <Stack screenOptions={{ headerShown: false }} />
+        </TransactionsProvider>
+      </NetworkGuard>
     </SafeAreaProvider>
   );
 }
